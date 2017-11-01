aa3c7f022fb0bc209c5f47f2a77f8203b11bf506
yuki <yukkixsu8@yahoo.co.jp>
2017-11-01 12:51:21 +0900

    static_pages作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  root 'static_pages#home'
   get 'static_pages/home'
-
   get 'static_pages/help'
-
+  get 'static-pages/about'
+  root 'static/home'
   resources :photos, except: [:new]
-  root :to => 'photos#index'
+  get 'photos/index'
 end

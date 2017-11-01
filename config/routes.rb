5a8c2ffbf3e7ef0893a0a43f9014821c938cdd36
yuki <yukkixsu8@yahoo.co.jp>
2017-11-01 16:36:10 +0900

    layouts,toutes編集とUsers Controller作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
-  get 'static_pages/home'
-  get 'static_pages/help'
-  get 'static-pages/about'
-  root 'static/home'
+  get '/home' => 'static_pages#home'
+  get '/help' => 'static_pages#help'
+  get '/about' => 'static_pages#about'
+  get '/contact' => 'static_pages#contact'
+  get '/signup' => 'users#new'
   resources :photos, except: [:new]
-  get 'photos/index'
+    get 'photos/index'
 end

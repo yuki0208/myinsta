2df5c43c04e491b07dc05a6fc807a2ffdd126d9e
yuki <yukkixsu8@yahoo.co.jp>
2017-11-28 00:16:03 +0900

    いいねボタンといいね数つけた

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,15 +1,24 @@
 class LikesController < ApplicationController
 
-  def create
-    @photopost = Photopost.find(params[:photopost_id])
-    @like = current_user.likes.create(photopost_id: params[:photopost_id])
-    render 'likes/create'
+  before_action :set_variables
+
+  def like
+    like = current_user.likes.new(photopost_id: @photopost.id)
+    like.save!
   end
 
-  def destroy
-    @photopost = Photopost.find(params[:photopost_id])
-    like = current_user.likes.find_by(photopost_id: params[:photopost_id])
-    like.destroy if like
-    render 'likes/create'
+  def unlike
+    like = current_user.likes.find_by(photopost_id: @photopost.id)
+    like.destroy!
   end
+
+  private
+
+   def set_variables
+     @photopost = Photopost.find(params[:photopost_id])
+     @id_name = "#like-link-#{@photopost.id}"
+     @id_heart = "#heart-#{@photopost.id}"
+     Rails.logger.info @id_name
+     Rails.logger.info @id_heart
+   end
 end

604d825fab2d80c1a3d4710f5e4f42cad0acfb64
yuki <yukkixsu8@yahoo.co.jp>
2017-12-12 22:13:52 +0900

    inquiryコントローラーモデルつくった

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171121073707) do
+ActiveRecord::Schema.define(version: 20171212114738) do
+
+  create_table "inquiries", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "likes", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer "user_id"
